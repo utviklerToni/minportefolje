24db353dbc8b24ed2529113d71cf856774765f10
utviklerToni <h1de.toni@protonmail.com>
2022-01-23 12:01:30 +0100

    working on project screen GUI

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './aboutMe.css';
+import "./aboutMe.css";
 
 const AboutMe = () => {
-	return (
-		<div style={{ height: '50vh' }}>
-			<div
-				className='raised_card aboutMe'
-				style={{ backgroundColor: '#1D3A41' }}>
-				<h1>About me</h1>
-				<p>
-					I am a full stack dev, I make web app on MERN stack, I also work on
-					electron.js and I create apps for Linux using Electron.js which is a
-					cross platform framework
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div style={{ height: "50vh" }}>
+      <div
+        className="raised_card aboutMe"
+        style={{ backgroundColor: "#1D3A41" }}
+      >
+        <h1>About me</h1>
+        <ul>
+          <li style={{ marginTop: "9px" }}>
+            <h3>
+              Front-end Developer with a bachelore degree in Computer Science
+            </h3>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
--- src/components/ProjectScreen/ProjectScreen.jsx
+++ src/components/ProjectScreen/ProjectScreen.jsx
@@ -1,46 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import PSComponent from './PSComponent';
-import signIn from '../../assets/images/signIn.webp';
-import bbbsHomePage from '../../assets/images/bbbs-mainpage-scaled04.webp';
-import homepagePic1 from '../../assets/images/homepage-fullhd.webp';
-import mobileGaialivsstil from '../../assets/images/mobileGaialivsstil.webp';
-import Gaialivsstil from './Gaialivsstil';
+import PSComponent from "./PSComponent";
+import signIn from "../../assets/images/signIn.webp";
+import bbbsHomePage from "../../assets/images/bbbs-mainpage-scaled04.webp";
+import homepagePic1 from "../../assets/images/homepage-fullhd.webp";
+import mobileGaialivsstil from "../../assets/images/mobileGaialivsstil.webp";
+import Gaialivsstil from "./Gaialivsstil";
 
 const ProjectScreen = () => {
-	return (
-		<Fragment>
-			<Gaialivsstil projectImage={homepagePic1} />
-			<PSComponent
-				projectImage={mobileGaialivsstil}
-				projectHeader={`Mobile view of Gaialivsstil`}
-				projectLink={`https://gaialivsstil.no/`}
-				projectLinkDescription={`Explore Gaialivsstil`}
-				projectDescriptionTitle={`About Gaialivsstil	`}
-				projectDescriptionParagraph={`This is my first project as a developer. A project for my Norwegian client.`}
-			/>
+  return (
+    <Fragment>
+      <Gaialivsstil projectImage={homepagePic1} />
+      <PSComponent
+        projectImage={mobileGaialivsstil}
+        projectHeader={`Mobile view of Gaialivsstil`}
+        projectLink={`https://gaialivsstil.no/`}
+        projectLinkDescription={`Explore Gaialivsstil`}
+        projectDescriptionTitle={`About Gaialivsstil	`}
+        projectDescriptionParagraph={`This is my first project as a developer. A project for my Norwegian client.`}
+      />
 
-			<PSComponent
-				projectImage={bbbsHomePage}
-				projectImgAlt={`image`}
-				projectHeader={`A restaurant project for Gaialivsstil`}
-				projectLink={`https://bjornsbeersandbones.herokuapp.com`}
-				projectLinkDescription={`Live demo here`}
-				projectDescriptionTitle={`About this project`}
-				projectDescriptionParagraph={` A simple restaurant website using React.js & redux state management with Node.js at the backend. Its a project for my client to support local organic farmers and serve their local community. This restaurant is set to open in beautiful town of Kragerø. It is one of the ongoing project I've been working on. The goal is to make it scalable using micro-service so it can be scaled. Right now I have developed it as a monolithic web-app.`}
-			/>
-			<PSComponent
-				projectImage={signIn}
-				projectImgAlt={`login project img`}
-				projectHeader={`Authentication with Redux`}
-				projectLink={`https://bjornsbeersandbones.herokuapp.com/login`}
-				projectLinkDescription={`Click here to try`}
-				projectDescriptionTitle={`About this project`}
-				projectDescriptionParagraph={` A simple authetication using redux state management with React.js. Redux is a predictable state container for
+      <PSComponent
+        projectImage={bbbsHomePage}
+        projectImgAlt={`image`}
+        projectHeader={`A restaurant project for Gaialivsstil`}
+        projectLink={`https://bjornsbeersandbones.herokuapp.com`}
+        projectLinkDescription={`Live demo here`}
+        projectDescriptionTitle={`About this project`}
+        projectDescriptionParagraph={` A simple restaurant website using React.js & redux state management with Node.js at the backend. Its a project for my client to support local organic farmers and serve their local community. This restaurant is set to open in beautiful town of Kragerø. It is one of the ongoing project I've been working on. The goal is to make it scalable using micro-service so it can be scaled. Right now I have developed it as a monolithic web-app.`}
+      />
+      <PSComponent
+        projectImage={signIn}
+        projectImgAlt={`login project img`}
+        projectHeader={`Authentication with Redux`}
+        projectLink={`https://bjornsbeersandbones.herokuapp.com/login`}
+        projectLinkDescription={`Click here to try`}
+        projectDescriptionTitle={`About this project`}
+        projectDescriptionParagraph={` A simple authetication using redux state management with React.js. Redux is a predictable state container for
         JavaScript apps. It helps you write applications that behave consistently, run in different environments (client, server, and native)	, and are easy to test. On top of that, it provides a great developer experience, such as live code editing combined with a time traveling debugger.`}
-			/>
-		</Fragment>
-	);
+      />
+    </Fragment>
+  );
 };
 
 export default ProjectScreen;
